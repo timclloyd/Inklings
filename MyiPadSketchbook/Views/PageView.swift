dbcb278e2dd8bda2fd56d71499c724ae1ad0f004
Tim Lloyd <git@timlloyd.me>
2024-09-11 20:35:49 +0100

    Refactored button style in MapView and PageView, included Map button in MapView, and optically aligned the icons

--- MyiPadSketchbook/Views/PageView.swift
+++ MyiPadSketchbook/Views/PageView.swift
@@ -212,16 +212,3 @@ struct PageView: View {
         )
     }
 }
-
-struct ToolbarButtonStyle: ButtonStyle {
-    @Environment(\.colorScheme) var colorScheme
-    var isEnabled: Bool
-
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .background(Circle().fill((colorScheme == .dark ? Color.black : Color.white)))
-            .foregroundColor(isEnabled ? .primary.opacity(0.87) : .primary.opacity(0.2))
-            .scaleEffect(configuration.isPressed ? 1.2 : 1.0)
-            .animation(.easeInOut(duration: 0.2), value: configuration.isPressed)
-    }
-}
