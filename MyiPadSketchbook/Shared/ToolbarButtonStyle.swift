8020249c2967e5588cf1cd648adda84bbdf05831
Tim Lloyd <git@timlloyd.me>
2024-10-12 13:06:21 +0100

    Buttons now grey out when not selected

--- MyiPadSketchbook/Shared/ToolbarButtonStyle.swift
+++ MyiPadSketchbook/Shared/ToolbarButtonStyle.swift
@@ -13,13 +13,16 @@ let toolbarButtonSize: CGFloat = 20
 struct ToolbarButtonStyle: ButtonStyle {
     @Environment(\.colorScheme) var colorScheme
     var isEnabled: Bool
+    var color: Color? = nil
 
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .foregroundColor(
-                isEnabled
-                    ? .primary.opacity(configuration.isPressed ? 0.57 : 0.87) // Enabled
-                    : .primary.opacity(0.2) // Disabled
+                (color ?? .primary).opacity(
+                    isEnabled
+                        ? (configuration.isPressed ? 0.57 : 0.87) // Enabled
+                        : 0.2 // Disabled
+                )
             )
             .animation(.easeInOut, value: isEnabled)
     }
